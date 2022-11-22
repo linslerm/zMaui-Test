7a1a93553ef6fc186e417ec8696d5ad218d81d9a
linslerm <34544019+linslerm@users.noreply.github.com>
2022-11-22 21:45:56 +0100

    .net 7 oncreate window

--- MauiApp1/App.xaml.cs
+++ MauiApp1/App.xaml.cs
@@ -8,4 +8,13 @@ public partial class App : Application
 
 		MainPage = new AppShell();
 	}
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        // Get display size
+        var displayInfo = DeviceDisplay.Current.MainDisplayInfo; // Center the window
+        var window =  base.CreateWindow(activationState);
+        window.X = (displayInfo.Width / displayInfo.Density - window.Width) / 2; window.Y = (displayInfo.Height / displayInfo.Density - window.Height) / 2;
+        return window;
+    }
 }
